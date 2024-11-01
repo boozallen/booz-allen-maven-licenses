df119a1a252a12b6d4f1329fa8dfcc990acd1c5f
Ryan Ashcraft <ryan.ashcraft@codifyiq.com>
2024-11-01 14:52:50 -0400

    #5 :bug: fix ambiguous unicode and trailing whitespace from license headers

--- booz-allen-licenses/src/license/com/boozallen/closed-source-license/header.txt.ftl
+++ booz-allen-licenses/src/license/com/boozallen/closed-source-license/header.txt.ftl
@@ -1,4 +1,4 @@
-All Rights Reserved. You may not copy, reproduce, distribute, publish, display, 
-execute, modify, create derivative works of, transmit, sell or offer for resale, 
-or in any way exploit any part of this solution without Booz Allen Hamilton’s 
+All Rights Reserved. You may not copy, reproduce, distribute, publish, display,
+execute, modify, create derivative works of, transmit, sell or offer for resale,
+or in any way exploit any part of this solution without Booz Allen Hamilton's
 express written permission.
--- license-example-projects/limited-government-use-examples/booz-allen-limited-government-use-python/src/booz_allen_limited_government_use_python/helloworld.py
+++ license-example-projects/limited-government-use-examples/booz-allen-limited-government-use-python/src/booz_allen_limited_government_use_python/helloworld.py
@@ -9,7 +9,7 @@
 # protection under the laws of the United States and other countries. You may not, copy,
 # reproduce, distribute, publish, display, execute, modify, create derivative works of,
 # transmit, sell or offer for resale, or in any way exploit any part of this solution
-# without Booz Allen Hamilton’s express written permission. This solution is subject to
+# without Booz Allen Hamilton's express written permission. This solution is subject to
 # the license restrictions noted in Contract No. C1234567890,
 # License No. L-9876321. This solution may be used only within
 # Ministry of Magic.  This solution may not be used or disclosed outside the
